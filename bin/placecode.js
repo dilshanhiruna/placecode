c7a0d40ef5c9856972136420bc8848b560bd2146
dilshanhiruna <dilshan.hirunaa@gmail.com>
2023-04-09 18:17:19 +0530

    bin placecode copy paste

--- bin/placecode.js
+++ bin/placecode.js
@@ -3,14 +3,38 @@
 const fs = require("fs");
 const path = require("path");
 const { exec } = require("child_process");
+const os = require("os");
 
-const folderName = "output"; // Change this to the name of the folder you want to copy
+const folderName = "zplacecode"; // Change this to the name of the folder you want to copy
 
 const sourceFolder = path.join(process.cwd(), folderName);
 const destFolder = path.join(process.cwd(), `${folderName}_copy`);
 
-// Use the 'cp' command to copy the folder
-exec(`cp -r ${sourceFolder} ${destFolder}`, (err, stdout, stderr) => {
-  if (err) throw err;
-  console.log(`Folder '${folderName}' copied to '${destFolder}'`);
-});
+function copyDirectory(source, destination) {
+  return new Promise((resolve, reject) => {
+    let command;
+    if (os.platform() === "win32") {
+      command = `xcopy "${source}" "${destination}" /E /I /Y`;
+    } else {
+      command = `cp -r "${source}" "${destination}"`;
+    }
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+async function main() {
+  try {
+    await copyDirectory(sourceFolder, destFolder);
+    console.log(`Folder '${folderName}' copied to '${destFolder}'`);
+  } catch (error) {
+    console.error("Error copying folder:", error);
+  }
+}
+
+main();
